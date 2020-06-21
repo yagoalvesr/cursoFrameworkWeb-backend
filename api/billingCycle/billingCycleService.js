5e68414e5928a653de0cb51e74814f49f171a7de
Yago Alves Ribeiro <yago.alves.r@hotmail.com>
2020-06-21 00:12:09 -0300

    ajustes nos validadores

--- api/billingCycle/billingCycleService.js
+++ api/billingCycle/billingCycleService.js
@@ -1,5 +1,5 @@
 const BillingCycle = require('./billingCycle')
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
-
+BillingCycle.updateOptions({new: true, runValidators: true})
 module.exports = BillingCycle
