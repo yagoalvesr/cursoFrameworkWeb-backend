7e2e84c26ac46506834b65db8f6fe70fa2b240b1
Yago Alves Ribeiro <yago.alves.r@hotmail.com>
2020-06-21 01:03:13 -0300

    implementacao do count

--- api/billingCycle/billingCycleService.js
+++ api/billingCycle/billingCycleService.js
@@ -2,4 +2,15 @@ const BillingCycle = require('./billingCycle')
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({new: true, runValidators: true})
+
+BillingCycle.route('count', function (req, res, next) {
+    BillingCycle.count(function (error, value) {
+        if (error) {
+            res.status(500).json({erros: [error]})
+        } else {
+            res.json({value})
+        }
+    })
+})
+
 module.exports = BillingCycle
